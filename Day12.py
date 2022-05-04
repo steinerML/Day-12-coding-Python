5706c88161e015c944e0ad79801d05db6e488011
steinerML <102678863+steinerML@users.noreply.github.com>
2022-05-04 13:03:35 +0200

    Add files via upload

---
+++ Day12.py
@@ -0,0 +1,18 @@
+# printing and rounding using round()
+
+e = 2.7182818
+
+print (round (e))
+
+print (round (e,2))
+
+print (round (e,1))
+
+print (int (e))
+
+print (float (round(e,2)))
+print (float (round(e,3)))
+print (float (round(e,4)))
+print (float (round(e,5)))
+print (float (round(e,6)))
+print (float (round(e,7)))
